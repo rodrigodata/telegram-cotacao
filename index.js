7e5c0ee7d096adc7bd7994d9cca61802000add0d
Rodrigo Andrade <datarodrigoandrade@gmail.com>
2019-03-26 00:43:07 -0300

    Remoção de pasta node_modules/ e inserção de arquivo do .gitignore

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-'use strict';
-var http = require('http');
-var port = (process.env.PORT || 5000);
-require = require('./bot.js')();
-
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.write(JSON.stringify({name: 'cambio_bot', ver: '0.0.1'}));
+"use strict";
+var http = require("http");
+var port = process.env.PORT || 5000;
+require = require("./bot.js")();
+/* */
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ name: "cambio_bot", ver: "0.0.1" }));
     response.end();
-}).listen(port);
+  })
+  .listen(port);
